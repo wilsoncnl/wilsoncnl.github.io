53898ac57cd3c845f115726566b4f0636bcf8270
wilsoncnl <107209197+wilsoncnl@users.noreply.github.com>
2022-06-10 01:33:46 +0800

    Add files via upload

---
+++ phpLearn/data.js
@@ -0,0 +1,54 @@
+var defaultThreads = [
+    {
+        id: 1,
+		
+        title1: "Undergo",
+		title:"Undergo",
+        author: "Aaron",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Jack",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Arthur",
+                date: Date.now(),
+                content: "Hey to you too"
+            }
+        ]
+    },
+    {
+        id: 2,
+		
+       
+		title1: "Feeling",
+		title: "Feeling",
+        author: "Aaron",
+        date: Date.now(),
+        content: "Thread content 2",
+        comments: [
+            {
+                author: "Jack",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Arthur",
+                date: Date.now(),
+                content: "Hey to you too"
+            }
+        ]
+    }
+]
+
+var threads = defaultThreads
+
+if (localStorage && localStorage.getItem('threads')) {
+    threads = JSON.parse(localStorage.getItem('threads'));
+} else {
+    threads = defaultThreads;
+    localStorage.setItem('threads', JSON.stringify(defaultThreads));
+}
---
+++ phpLearn/welcome.php
@@ -0,0 +1,4 @@
+<?php
+　$YourName=$_POST[YourName];
+　echo '接收到的內容為: '.$YourName;
+?>
